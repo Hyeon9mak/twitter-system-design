0da049edd8267a08072f39b03f3c012828f55581
Hyeon9mak <jinha3507@gmail.com>
2023-02-05 16:28:44 +0900

    feat: 인플루언서 오바마 데이터 추가

--- src/main/kotlin/hyeon9mak/twittersystemdesign/config/DatabaseInitializer.kt
+++ src/main/kotlin/hyeon9mak/twittersystemdesign/config/DatabaseInitializer.kt
@@ -1,7 +1,6 @@
 package hyeon9mak.twittersystemdesign.config
 
-import hyeon9mak.twittersystemdesign.domain.Follow
-import hyeon9mak.twittersystemdesign.domain.FollowRepository
+import hyeon9mak.twittersystemdesign.service.FollowService
 import hyeon9mak.twittersystemdesign.service.TweetService
 import hyeon9mak.twittersystemdesign.service.UserService
 import org.springframework.boot.CommandLineRunner
@@ -12,19 +11,31 @@ import org.springframework.transaction.annotation.Transactional
 @Component
 class DatabaseInitializer(
     private val userService: UserService,
-    private val followRepository: FollowRepository,
+    private val followService: FollowService,
     private val tweetService: TweetService,
 ) : CommandLineRunner {
 
     override fun run(vararg args: String?) {
-        userService.registerUser(id = 1L, username = "user1", profilePic = "user1.png")
+        initUser1()
+        initObama()
+    }
 
+    private fun initUser1() {
+        userService.registerUser(id = 1L, username = "user1", profilePic = "user1.png")
         for (i: Long in 2L..100L) {
             userService.registerUser(id = i, username = "user$i", profilePic = "user$i.png")
-            followRepository.save(Follow(followerId = 1L, followeeId = i))
+            followService.follow(followerId = 1L, followeeId = i)
             tweetService.postTweet(senderId = i, text = "user$i's first tweet")
             tweetService.postTweet(senderId = i, text = "user$i's second tweet")
             tweetService.postTweet(senderId = i, text = "user$i's third tweet")
+        }
+    }
+
+    private fun initObama() {
+        userService.registerUser(id = 101L, username = "obama", profilePic = "obama.png")
+        for (i: Long in 102L..13_102L) {
+            userService.registerUser(id = i, username = "user$i", profilePic = "user$i.png")
+            followService.follow(followerId = i, followeeId = 101L)
         }
     }
 }
