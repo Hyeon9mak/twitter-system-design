57232e9b496606f5bb88045b6c5fc3b5a2a895e2
hyeon9mak <jinha3507@gmail.com>
2023-02-06 23:19:22 +0900

    chore: 성능 테스트를 위한 k6 스크립트 작성

---
+++ k6-script.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+
+export const options = {
+  vus: 1,
+  duration: '60s',
+};
+
+export default function () {
+  http.batch([
+    ['GET', 'http://localhost:8080/api/v1/feeds?user-id=1', null, null],
+    ['GET', 'http://localhost:8080/api/v1/feeds?user-id=2', null, null],
+  ])
+}
