b95c9e3075848fadaba60393967dd0ff85042a79
hyeon9mak <jinha3507@gmail.com>
2023-02-08 09:08:42 +0900

    refactor: 코드레벨에 redis host, port 설정 기입

--- src/main/kotlin/hyeon9mak/twittersystemdesign/config/RedisConfig.kt
+++ src/main/kotlin/hyeon9mak/twittersystemdesign/config/RedisConfig.kt
@@ -1,6 +1,7 @@
 package hyeon9mak.twittersystemdesign.config
 
 import hyeon9mak.twittersystemdesign.domain.Feed
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -8,10 +9,13 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate
 
 @Configuration
-class RedisConfig {
+class RedisConfig(
+    @Value("\${spring.redis.host}") private val host: String,
+    @Value("\${spring.redis.port}") private val port: Int,
+) {
 
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory()
+    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(host, port)
 
     @Bean
     fun redisTemplate(): RedisTemplate<Long, Feed> {
