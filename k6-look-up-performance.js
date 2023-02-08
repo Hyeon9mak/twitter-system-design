98de1dcea541d2804094b83ab8b7b7e73f3e20be
hyeon9mak <jinha3507@gmail.com>
2023-02-08 10:04:07 +0900

    refactor: fan out 성능 테스트를 위한 값 수정

--- k6-look-up-performance.js
+++ k6-look-up-performance.js
@@ -2,12 +2,12 @@ import http from 'k6/http';
 
 export const options = {
   vus: 1,
-  duration: '10s',
+  duration: '60s',
 };
 
 export default function () {
   http.batch([
-    ['GET', 'http://localhost:8080/api/v1/feeds?user-id=1', null, null],
-    ['GET', 'http://localhost:8080/api/v1/feeds?user-id=2', null, null],
+    ['GET', 'http://localhost:8080/api/v1/feeds?user-id=10002', null, null],
+    ['GET', 'http://localhost:8080/api/v1/feeds?user-id=10003', null, null],
   ])
 }
--- src/main/kotlin/hyeon9mak/twittersystemdesign/config/DatabaseInitializer.kt
+++ src/main/kotlin/hyeon9mak/twittersystemdesign/config/DatabaseInitializer.kt
@@ -17,41 +17,47 @@ class DatabaseInitializer(
 ) : CommandLineRunner {
 
     override fun run(vararg args: String?) {
-        register100users()
+        register10_000users()
+        initObama()
         initUserA()
         initUserB()
-        initObama()
-        tweet100Users()
+        tweet10_000Users()
+        obamaTweet()
     }
 
-    private fun register100users() {
-        for (i: Long in 1L..100L) {
+    private fun register10_000users() {
+        for (i: Long in 1L..10_000L) {
             userService.registerUser(id = i, username = "user$i", profilePic = "user$i.png")
         }
     }
 
+    private fun initObama() {
+        userService.registerUser(id = 10_001L, username = "obama", profilePic = "obama.png")
+        for (i: Long in 1L..10_000L) {
+            followRepository.save(Follow(followerId = i, followeeId = 10_001L))
+        }
+    }
+
     private fun initUserA() {
-        userService.registerUser(id = 101L, username = "userA", profilePic = "userA.png")
-        for (i: Long in 1L..100L) {
-            followRepository.save(Follow(followerId = 101L, followeeId = i))
+        userService.registerUser(id = 10_002L, username = "userA", profilePic = "userA.png")
+        for (i: Long in 1L..10_001L) {
+            followRepository.save(Follow(followerId = 10_002L, followeeId = i))
         }
     }
 
     private fun initUserB() {
-        userService.registerUser(id = 102L, username = "userB", profilePic = "userB.png")
-        followRepository.save(Follow(followerId = 102L, followeeId = 1L))
+        userService.registerUser(id = 10_003L, username = "userB", profilePic = "userB.png")
+        followRepository.save(Follow(followerId = 10_003L, followeeId = 1L))
+        followRepository.save(Follow(followerId = 10_003L, followeeId = 10_001L))
     }
 
-    private fun initObama() {
-        userService.registerUser(id = 103L, username = "obama", profilePic = "obama.png")
-        for (i: Long in 1L..102L) {
-            followRepository.save(Follow(followerId = i, followeeId = 103L))
-        }
-    }
-
-    private fun tweet100Users() {
-        for (i: Long in 1L..100L) {
+    private fun tweet10_000Users() {
+        for (i: Long in 1L..10_000L) {
             tweetService.postTweet(senderId = i, text = "user$i's tweet")
         }
+    }
+
+    private fun obamaTweet() {
+        tweetService.postTweet(senderId = 10_001L, text = "obama's tweet")
     }
 }
